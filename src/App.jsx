034155925ab61a8e88db1f620c88b36a57bc0b67
raihanhn <raihanhn20@gmail.com>
2023-01-26 02:51:46 +0600

    7th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import {Navbar, Hero, Footer} from './components'
-import { hero, navlinks } from './data/travigodata'
+import {Navbar, Hero, Footer, Memory} from './components'
+import { hero, navlinks, memory } from './data/travigodata'
 
 const App = () => {
   return (
     <>
       <Navbar navlinks={navlinks} />
       <Hero hero={hero} />
+      <Memory memory={memory} />
       <Footer/>
     </>
   )
