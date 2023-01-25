21eb930f992e61dcbe35aee9a6106cac08f78eee
raihanhn <raihanhn20@gmail.com>
2023-01-25 21:04:10 +0600

    5th commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../images/logo.png'
 
-const Navbar = () => {
+const Navbar = ({navlinks}) => {
   return (
-    <div>Navbar</div>
+    <>
+      <header className='flex items-center justify-center w-auto h-auto absolute top-7 left-0 right-0 '>
+        <nav className='flex items-center justify-between travigo-container ' >
+          <NavLink to={`/`} className='flex items-center ' >
+            <img src={logo} alt="logo/img" className='w-22 h-9 object-fill ' />
+          </NavLink>
+          <ul className='flex items-center lg:hidden gap-7 ' >
+            {navlinks?.map((val, i) => (
+              <li key={i}><NavLink to={'#'} className='text-lg text-slate-900 '> {val.link} </NavLink> </li>
+            ))}
+          </ul>
+          <ul className='flex items-center lg:hidden '><li>
+              <button type='button' className='button-emrald px-7 text-base ' >Join Us</button>  
+          </li></ul>
+        </nav>
+      </header>
+    </>
   )
 }
 
