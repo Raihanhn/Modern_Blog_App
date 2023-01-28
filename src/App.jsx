8668b593b1c7dc24746963d556a815c915e402c4
raihanhn <raihanhn20@gmail.com>
2023-01-28 21:48:02 +0600

    11th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Navbar, Hero, Footer, Memory, Explore, Advertise} from './components'
-import { hero, navlinks, memory, placesAPI, brands } from './data/travigodata'
+import {Navbar, Hero, Footer, Memory, Explore, Advertise, Pricings} from './components'
+import { hero, navlinks, memory, placesAPI, brands, pricingapi } from './data/travigodata'
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
       <Memory memory={memory} />
       <Explore title="Explore The Beauty of World" placesAPI={placesAPI} />
       <Advertise brands={brands} />
+      <Pricings pricingapi={pricingapi} />
       <Footer/>
     </>
   )
