7d6c7cd16f57b0a2d295dbe2af2d9d4b30987068
raihanhn <raihanhn20@gmail.com>
2023-01-29 16:55:35 +0600

    added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Modern_Travel_Agency/'
+  base: "/Modern_Travel_Agency/"
 })
